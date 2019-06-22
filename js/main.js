25780463774965321edeae517ec7df5b5bf18fc2
hetpatel07 <hets7878401023@gmail.com>
2019-06-22 16:32:14 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -1,55 +1,51 @@
 //value part
 var i = 0;
 var width = $(document).width();
-var height = $(document).height();
+console.log($(window).height());
+var height = $('#main').height();
+
 
 var x = width / 360;
 var y = height / 100;
-var z = 1468 / 100;
+var z = 820 / 100;
 console.log(x);
 
-var hue = 136;
+var hue = 236;
 var sat = 65 + '%';
 var light = 63 + '%';
 var mainHeight = $('#main').css('height');
 mainHeight = parseInt(mainHeight);
 console.log(mainHeight);
 var m = 0;
-$(document).mousemove(function () {
-    hue = Math.round(event.pageX / x);
-    sat = Math.round(event.pageY / y) + '%';
+$(document).scroll(function (event) {
+    light = Math.round(scrollY / z) + '%';
+    console.log(light);
     var hsl = 'hsl(' + hue + ', ' +
-        sat + ',' + light + ')';
+        light + ',' + sat + ')';
     console.log(hsl);
     $('#main').css({
         "background-color": hsl,
     })
 });
-$(window).scroll(function (event) {
-    //    var st = $(this).scrollTop();
-    //    if (st > lastScrollTop) {
-    //
-    //
-    //    } else {
-    //        i--;
-    //        console.log(i);
-    //    }
-    //    lastScrollTop = st;
-
-    //    light = Math.round(10000/mainHeight) + '%';
-    //    m++;
-    light = Math.round(scrollY / z) + '%';
-    console.log(light);
+$('#main').mousemove(function () {
+    hue = Math.round(event.pageX / x);
+    
+    var rect = event.target.getBoundingClientRect();
+    var m = event.clientY - rect.top;
+    console.log(m)
+    /**/
+    sat = Math.round(m / y) + '%';
+    
     var hsl = 'hsl(' + hue + ', ' +
-        sat + ',' + light + ')';
+        light + ',' + sat + ')';
+    console.log(hsl);
     $('#main').css({
         "background-color": hsl,
     })
-
 });
 
 
 i = 0;
 var lastScrollTop = 0;
 
-var rgb = $(".main").css('background-color');
+//var rgb = $(".main").css('background-color');
