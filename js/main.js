53faca3406545a0930502c6d771ae88a29cbaf48
hetpatel07 <hets7878401023@gmail.com>
2019-07-16 16:46:43 +0530

    updated

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 //console.log($(window).height());
 var x;
 var y;
+var rgb;
 
 function hw() {
     var width = $('#main').innerWidth();
-    console.log(width);
+
     var height = $('#main').innerHeight();
     x = width / 360;
     y = height / 100;
@@ -14,14 +15,12 @@ var i = 0;
 var z = 1444 / 100;
 var h;
 var s;
-var l = 63 + '%';
+var l = 0 + '%';
 var mainHeight = $('#main').css('height');
 mainHeight = parseInt(mainHeight);
-
-console.log(mainHeight);
 var m = 0;
 var hsl;
-
+var hex;
 $(document).scroll(function (event) {
     l = Math.round(scrollY / z) + '%';
     hsl = 'hsl(' + hue + ', ' +
@@ -30,6 +29,7 @@ $(document).scroll(function (event) {
         "background-color": hsl,
     });
 });
+
 $('#main').mousemove(function (event) {
     hw();
     var relX = event.pageX - $(this).offset().left;
@@ -38,32 +38,28 @@ $('#main').mousemove(function (event) {
     var rect = event.target.getBoundingClientRect();
     var m = event.clientY - rect.top;
     s = Math.round(m / y);
+    var light = parseInt(l);
     hsl = 'hsl(' + hue + ', ' +
         l + ' ,' + s + '% )';
-    var light = parseInt(l);
-    //    console.log(hsl);
     $('#main').css({
         "background-color": hsl,
     });
-    $('.content').html(HSLToHex(h, light, s));
+
+    rgb = $('#main').css('backgroundColor');
+
+    $('.content').html(rgb2hex(rgb));
 });
 
 $('#main').click(function () {
     var newDiv = document.createElement('div');
     newDiv.setAttribute('class', 'onclick col');
+    newDiv.setAttribute('id', 'onclick');
     var mainWidth = $('#main').innerWidth() / 3;
     console.log(mainWidth);
     var row = document.getElementsByClassName('row')[0];
     row.insertBefore(newDiv, document.getElementById('main'));
     newDiv.style.background = hsl;
-    //    $('.onclick').css('width', mainWidth + 'px');
-
-
-    //    var light = parseInt(l);
-    //    var contentDiv = document.createElement('div');
-    //    contentDiv.setAttribute('class', 'contentinner text-center');
-    //    newDiv.appendChild(contentDiv);
-    //    contentDiv.innerHTML = document.getElementById('content').innerHTML;
+    console.log(hsl);
 
     newDiv.setAttribute('data-change', document.getElementById('content').innerHTML);
     //    newDiv.setAttribute('data-cancel', '<i class="far fa-window-close" onclick="my(this)"></i>');
@@ -74,62 +70,29 @@ $('#main').click(function () {
     cancelBtn.innerHTML = '<i class="far fa-window-close"></i>';
 
     cancelBtn.setAttribute('onclick', 'my(this)');
+    $(".onclick").click(function () {
+        console.log($('.onclick').attr('data-change'));
+    });
+
 });
 
 function my(e) {
     console.log(e.parentElement);
     e.parentElement.remove();
 }
-//hsl to rgb converter
+//rgb to hex converter
 
-function HSLToHex(h, s, l) {
-    s /= 100;
-    l /= 100;
 
-    let c = (1 - Math.abs(2 * l - 1)) * s,
-        x = c * (1 - Math.abs((h / 60) % 2 - 1)),
-        m = l - c / 2,
-        r = 0,
-        g = 0,
-        b = 0;
+function rgb2hex(rgb) {
+    if (rgb.search("rgb") == -1) {
+        return rgb;
+    } else {
+        rgb = rgb.match(/^rgba?\((\d+),\s*(\d+),\s*(\d+)(?:,\s*(\d+))?\)$/);
 
-    if (0 <= h && h < 60) {
-        r = c;
-        g = x;
-        b = 0;
-    } else if (60 <= h && h < 120) {
-        r = x;
-        g = c;
-        b = 0;
-    } else if (120 <= h && h < 180) {
-        r = 0;
-        g = c;
-        b = x;
-    } else if (180 <= h && h < 240) {
-        r = 0;
-        g = x;
-        b = c;
-    } else if (240 <= h && h < 300) {
-        r = x;
-        g = 0;
-        b = c;
-    } else if (300 <= h && h < 360) {
-        r = c;
-        g = 0;
-        b = x;
+        function hex(x) {
+            return ("0" + parseInt(x).toString(16)).slice(-2);
+        }
+
+        return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
     }
-    // Having obtained RGB, convert channels to hex
-    r = Math.round((r + m) * 255).toString(16);
-    g = Math.round((g + m) * 255).toString(16);
-    b = Math.round((b + m) * 255).toString(16);
-
-    // Prepend 0s, if necessary
-    if (r.length == 1)
-        r = "0" + r;
-    if (g.length == 1)
-        g = "0" + g;
-    if (b.length == 1)
-        b = "0" + b;
-
-    return "#" + r + g + b;
 }
